9096a7fda4df3ffae4652774765e51c97b936039
Jocelyne Jones <gothling@jocely.net>
2024-08-14 16:54:24 +0100

    setup button and state for menu

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,10 +1,25 @@
-import styles from "./Header.module.css"
+'use client'
+
+import Image from "next/image";
+import styles from "./Header.module.css";
+import { useState } from 'react';
 
 export default function Header(props) {
-
+  const [ menuState, setMenuState ] = useState(false);
+  function onHandleClick() {
+    setMenuState(!menuState);
+    console.log(menuState);
+  }
   return (
     <header>
     <h1>🔥{ props.title }</h1>
+    <button onClick={onHandleClick}>
+      <Image
+        src = '/assests Founders/menu-open-button.png'
+        width={30}
+        height={30}
+      />
+    </button>
    </header>
   )
 }
