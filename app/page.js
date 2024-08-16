3cbcc85a5b62ad5b8d6bcc024cff03ae7eb64e93
Stace0x <167257706+Stace0x@users.noreply.github.com>
2024-08-16 12:04:31 +0100

    reviews added

--- app/page.js
+++ app/page.js
@@ -4,13 +4,22 @@ import Image from "next/image";
 import "./page.css";
 import HeroText from '../src/Component/HeroText/HeroText.jsx'
 import Footer from "@/src/Component/Footer/Footer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 export default function Home() {
 
   const[selectedCountry, setSelectedCountry] = useState(null);
+  const[ countryData, setCountryData ] = useState(null);
+
+  useEffect(() => {
+    if (selectedCountry) {
+      fetch(`https://seal-app-336e8.ondigitalocean.app/reviews?country=${selectedCountry}`)
+        .then(response => response.json())
+        .then(data => setCountryData(data))
+    }
+  }, [selectedCountry])
 
   function inputCountry(country){
-    console.log(country);
+    setSelectedCountry(country);
   }
 
   return(
@@ -27,20 +36,20 @@ export default function Home() {
           <div>
             <section className = "reviews">
               <h2 className = "how-it-works-text">Trusted.</h2>
-              <hr></hr>
+              <hr className="solid"/>
               <p>We've got thousands of happy customers all over the UK. Choose your country to see the latest review:</p>
               <div className = "buttonContainer">
-                
-                <button className = "countryButton" onClick={()=>inputCountry("England")}>England</button>
-                <button className = "countryButton"  onClick={()=>inputCountry("Wales")}>Wales</button>
-                <button className = "countryButton"  onClick={()=>inputCountry("Scotland")}>Scotland</button>
+ {/* // if select country is england then its active, active is a class and css can be used on it  */}
+                <button className={`countryButton ${ selectedCountry === "England" ? "active" : " "}`} onClick={()=>inputCountry("England")}>England</button>
+                <button className={`countryButton ${ selectedCountry === "Wales" ? "active" : " "}`} onClick={()=>inputCountry("Wales")}>Wales</button>
+                <button className={`countryButton ${ selectedCountry === "Scotland" ? "active" : " "}`} onClick={()=>inputCountry("Scotland")}>Scotland</button>
               </div>
-              <div>
+              {countryData && ( <div>
                 <div className = "reviewsContainer">
-                  <p>I hated the fireplace. Booooo</p>
+                  <p>{countryData.text}</p>
                 </div>
-                <p className = "reviewerText">Unhappy person</p>
-              </div>
+                <p className = "reviewerText">{countryData.author} - {countryData.location}</p>
+              </div>)}
             </section>
           </div>
           <div className ="how-it-works-text">
