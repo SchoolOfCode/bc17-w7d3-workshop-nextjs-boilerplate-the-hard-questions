46277a42f98b5947e193cb4c304ad5c1a3863251
Shilpa <eclectic89@hotmail.com>
2024-08-20 11:21:13 +0100

    handle submit and handle change

--- app/booking/page.js
+++ app/booking/page.js
@@ -1,29 +1,51 @@
+'use client'
+
 import React from "react";  
 import './page.css'
+import { useState } from "react";
 
 export default function Booking() {
+
+  const [booking, setBooking] = useState({
+    name:'', 
+    postCode:'',
+    address:'',
+    city:'',
+    phoneNumber:'',
+    email:''
+  })
+  const handleChange = (event) => {
+    const {name, value} = event.target
+    setBooking((oldBooking)=>({...oldBooking, [name]: value}))
+  }
+  const handleSubmit = (event) =>{
+    event.preventDefault()
+    console.log(booking)
+  }
+
+
   return(
     <>
       <h1>Design Booking</h1>
-      <form action="/action_page.php">
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <legend>Personalia:</legend>
           <ul>
             <li>
               <label for="name">Full name:</label>
-              <input type="text" id="name" name="name"/>
+              <input type ="text" id="name" name="name" value={booking.name} onChange={handleChange}/>
             </li>
             <li>
               <label for="postCode">Postcode:</label>
-              <input type="text" id="postCode" name="postCode"/>
+              <input type="text" id="postCode" name="postCode" value={booking.postCode} onChange={handleChange}/>
             </li>
             <li>
               <label for="address">Address:</label>
-              <input type="text" id="address" name="address"/>
+              <input type="text" id="address" name="address" value={booking.address} onChange={handleChange}/>
             </li>
             <li>
               <label for="city">City:</label>
-              <input type="text" id="city" name="city"/>
+              <input type="text" id="city" name="city" value={booking.city} onChange={handleChange}/>
             </li>
           </ul>
         </fieldset>
@@ -32,11 +54,11 @@ export default function Booking() {
           <ul>
             <li>
               <label for="phoneNumber">Phone number:</label>
-              <input type="text" id="phoneNumber" name="phoneNumber"/>
+              <input type="text" id="phoneNumber" name="phoneNumber" value={booking.phoneNumber} onChange={handleChange}/>
             </li>
             <li>
               <label for="email">Email address:</label>
-              <input type="text" id="email" name="email"/>
+              <input type="text" id="email" name="email" value={booking.email} onChange={handleChange}/>
             </li>
           </ul>
         </fieldset>
