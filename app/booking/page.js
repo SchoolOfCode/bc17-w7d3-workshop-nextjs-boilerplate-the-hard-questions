b9840c020648fba978bf53770b8e1b3c1a6f8498
Jocelyne Jones <gothling@jocely.net>
2024-08-21 09:41:11 +0100

    minor tweak

--- app/booking/page.js
+++ app/booking/page.js
@@ -2,7 +2,6 @@
 
 import React from "react";
 import './page.css'
-import { useState } from "react";
 import BookingForm from "@/src/Component/BookingForm/BookingForm";
 
 export default function Booking() {
