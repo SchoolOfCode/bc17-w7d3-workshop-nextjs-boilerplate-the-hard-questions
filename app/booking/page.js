9e547fa049c729df0cb062b511a14d12dc536662
Stace0x <167257706+Stace0x@users.noreply.github.com>
2024-08-21 09:13:12 +0100

    formating html

--- app/booking/page.js
+++ app/booking/page.js
@@ -42,45 +42,47 @@ export default function Booking() {
 
   return (
     <>
-      <h1>Design Booking</h1>
-      <form onSubmit={handleSubmit}>
-        <fieldset>
-          <legend>Personalia:</legend>
-          <ul>
-            <li>
-              <label htmlFor="name">Full name:</label>
-              <input type="text" id="name" name="name" value={booking.name} onChange={handleChange} />
-            </li>
-            <li>
-              <label htmlFor="postCode">Postcode:</label>
-              <input type="text" id="postCode" name="postCode" value={booking.postCode} onChange={handleChange} />
-            </li>
-            <li>
-              <label htmlFor="address">Address:</label>
-              <input type="text" id="address" name="address" value={booking.address} onChange={handleChange} />
-            </li>
-            <li>
-              <label htmlFor="city">City:</label>
-              <input type="text" id="city" name="city" value={booking.city} onChange={handleChange} />
-            </li>
-          </ul>
-        </fieldset>
-        <fieldset>
-          <legend>Personalia:</legend>
-          <ul>
-            <li>
-              <label htmlFor="phoneNumber">Phone number:</label>
-              <input type="text" id="phoneNumber" name="phoneNumber" value={booking.phoneNumber} onChange={handleChange} />
-            </li>
-            <li>
-              <label htmlFor="email">Email address:</label>
-              <input type="text" id="email" name="email" value={booking.email} onChange={handleChange} />
-            </li>
-          </ul>
-        </fieldset>
-        {inputError && <p style={{ color: 'red' }}>{inputError}</p>}
-        <input type="submit" value="Submit" />
-      </form>
+      <h1 className="designBooking">Design Booking</h1>
+      <section className="formWrapper">
+          <form onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Personal Information:</legend>
+            <ul>
+              <li>
+                <label htmlFor="name">Full name:</label>
+                <input type="text" id="name" name="name" value={booking.name} onChange={handleChange} />
+              </li>
+              <li>
+                <label htmlFor="postCode">Postcode:</label>
+                <input type="text" id="postCode" name="postCode" value={booking.postCode} onChange={handleChange} />
+              </li>
+              <li>
+                <label htmlFor="address">Address:</label>
+                <input type="text" id="address" name="address" value={booking.address} onChange={handleChange} />
+              </li>
+              <li>
+                <label htmlFor="city">City:</label>
+                <input type="text" id="city" name="city" value={booking.city} onChange={handleChange} />
+              </li>
+            </ul>
+          </fieldset>
+          <fieldset>
+            <legend>Contact Information:</legend>
+            <ul>
+              <li>
+                <label htmlFor="phoneNumber">Phone number:</label>
+                <input type="text" id="phoneNumber" name="phoneNumber" value={booking.phoneNumber} onChange={handleChange} />
+              </li>
+              <li>
+                <label htmlFor="email">Email address:</label>
+                <input type="text" id="email" name="email" value={booking.email} onChange={handleChange} />
+              </li>
+            </ul>
+          </fieldset>
+          {inputError && <p style={{ color: 'red' }}>{inputError}</p>}
+          <input type="submit" value="Submit" />
+        </form>
+      </section>
     </>
   )
 }
