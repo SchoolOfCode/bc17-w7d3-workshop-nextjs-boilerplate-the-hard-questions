d9823d135612e6d9dbde654455effd445aa40590
Stace0x <167257706+Stace0x@users.noreply.github.com>
2024-08-23 11:15:38 +0100

    form success test added

--- tests/app.test.js
+++ tests/app.test.js
@@ -15,16 +15,31 @@ test('Menu button',async({page})=> {
 test('Form' ,async({page})=> {
   await page.goto('http://localhost:3000/booking');
   await page.getByLabel('Name').fill('tom');
-
+  await page.getByLabel('Postcode').fill('se19');
+  await page.getByLabel('Address').first().fill('home');
+  await page.getByText('Request Design Consultation').click();
+  await expect(page.getByText('All fields need to be filled in')).toBeVisible();
 });
 
+test('Form Success' ,async({page})=> {
+  await page.goto('http://localhost:3000/booking');
+  await page.getByLabel('Name').fill('tom');
+  await page.getByLabel('Postcode').fill('se19');
+  await page.getByLabel('Address').first().fill('home');
+  await page.getByLabel('City').first().fill('home');
+  await page.getByLabel('Phone number').first().fill('home');
+  await page.getByLabel('Email address').fill('home');
+  await page.getByText('Request Design Consultation').click();
+  await expect(page.getByText('Loading')).toBeVisible();
+  await expect(page.getByText('Submitted')).toBeVisible();
+});
 
 /*
 As a CUSTOMER I want to book a consultation.
 I want the page to load when I go to the website. ✅
 I want the menu button to be clickable and for it to show the menu. ✅
 I want to be able to navigate to the consultation page and for the menu to close and the correct URL to show.✅
-I want to be able to input the information into the form fields.
-I want to be informed if there is a problem with the form.
+I want to be able to input the information into the form fields.✅
+I want to be informed if there is a problem with the form.✅
 I want to see the status and confirmation of form submission.
 */
